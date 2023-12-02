c6207ad7f86794222e623151c67377de4598ec10
joaokristani <joaokristanikk@outlook.com>
2023-12-02 08:38:46 -0300

    Initial commit🙀

---
+++ src/main/java/org/xvideos/untitled/Untitled.java
@@ -0,0 +1,16 @@
+package org.xvideos.untitled;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Untitled extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("O plugin foi ativado com sucesso, ");
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
